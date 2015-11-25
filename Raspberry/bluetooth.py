6a2f7800b449ab952ae1af26bd3021d3393449d5
hersftw <hersir@gmail.com>
2015-11-25 19:43:20 +0000

    Changes made on Fiberparty

--- Raspberry/bluetooth.py
+++ Raspberry/bluetooth.py
@@ -2,13 +2,15 @@ import serial
 from time import sleep
  
 bluetoothSerial = serial.Serial( "/dev/rfcomm1", baudrate=9600 )
-
+  
 count = None
 while count == None:
 	try:
-		count = int(raw_input( "Please enter the number of times to blink the LED "))
+		dev = int(raw_input( "Please enter the device ID: "))
+		count = int(raw_input( "Please enter the state of the art: "))
 	except:
 		pass    # Ignore any errors that may occur and try again
-						   
-bluetoothSerial.write( str(count) )
+param = "dev"+str(dev)+":"+str(count)
+#bluetoothSerial.write( str(param) )
+bluetoothSerial.write(str(dev))
 print bluetoothSerial.readline()
