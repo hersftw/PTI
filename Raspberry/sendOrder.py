3798d12f8732e56af81692bfe0b50ed769baf4a2
hersftw <hersir@gmail.com>
2015-12-02 22:32:23 +0000

    Backup

--- Raspberry/sendOrder.py
+++ Raspberry/sendOrder.py
@@ -1,22 +1,34 @@
+#! /usr/bin/python
+
 import serial, cgi, os, re, sys, string, time, requests
 from time import sleep
 
 params = cgi.FieldStorage()
-device = params['device'].value
-state = params['state'].value
- 
+device = params['deviceID'].value
+state = params['newstate'].value
+
+print "Content-type: text/html\n\n"
+
+print "<p>"+device+"</p>"
+print "<p>"+state+"</p>"
+
 tempSerial = serial.Serial( "/dev/ttyUSB0", baudrate=9600)
 ledSerial = serial.Serial( "/dev/ttyACM0", baudrate=9600)
 
-sleep(2)
+#sleep(2)
 
 #res = device+":"+state
 #ledSerial.write(res)
+
 ledSerial.write(state)
-print ledSerial.readline()
+print "<p> Estat: "+ledSerial.readline()+"</p>"
 
 tempSerial.write("1") #Ask for the temperature
-temp = tempSerial.readline()
-print temp
+tempS = tempSerial.readline()
+print "<p>Temp string: "+tempS+"</p>"
+temp = int(float(tempS)+0.5)
 
-r = requests.post("http://fiberparty.upc.edu:8080/", data = {"temp":temp})
+print "<p> Temp: %d</p>"%temp
+
+payload = {"username":"pti", "password":"pti", "deviceID":"2", "newstate":temp}
+r = requests.post("http://fiberparty.upc.es:8080/PTIRestBackend/webresources/devicesstatepi", data = payload)
