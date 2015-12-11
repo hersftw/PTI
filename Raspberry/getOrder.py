3c4732574b898ea2a22a15779da81920fa7c4e40
hersftw <hersir@gmail.com>
2015-12-11 11:24:00 +0000

    Last changes

--- Raspberry/getOrder.py
+++ Raspberry/getOrder.py
@@ -1,12 +1,15 @@
 #! /usr/bin/python
-import serial, cgi, os, re, sys, string, requests
+import serial, cgi, os, re, sys, string, requests, subprocess
 
-alarmSerial = serial.Serial( "/dev/ttyUSB1", baudrate=9600)
+alarmSerial = serial.Serial( "/dev/ttyUSB0", baudrate=9600)
 
 while 1:
 	print "Waiting for the alarm to proc..."
-	activated = alarmSerial.readline()
-	print "Alarm activated! Recieved %s. Sending post request..."%activated
-	payload = {"username":"pti", "password":"pti", "deviceID":"", "newstate":activated}
-	r = requests.post("http://fiberparty.upc.es:8080/PTIRestBackend/webresources/devicesstatepi", data = payload)
-	print "Request sent! Going back to the listen state..."
+	activated = alarmSerial.read(1)
+	if activated == "2":
+		print "Alarm activated! Recieved %s. Sending mail..."%activated
+		os.system("echo 'PEASSOPENE' | mail -s 'ALARMMMM' joanrc93@gmail.com")
+#payload = {"username":"pti", "password":"pti", "deviceID":"", "newstate":activated}
+	#r = requests.post("http://fiberparty.upc.es:8080/PTIRestBackend/webresources/devicesstatepi", data = payload)
+		print "Request sent! Going back to the listen state..."
+	else: print "%s recieved, going back to the listen state..."%activated
--- Raspberry/sendOrder.py
+++ Raspberry/sendOrder.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import serial, cgi, os, re, sys, string, time, requests
+import serial, cgi, os, re, sys, string, time, requests, hashlib, random
 from time import sleep
 
 params = cgi.FieldStorage()
@@ -9,25 +9,44 @@ state = params['newstate'].value
 
 print "Content-type: text/html\n\n"
 
-print "<p>"+device+"</p>"
-print "<p>"+state+"</p>"
+#print "<p>"+device+"</p>"
+#print "<p>"+state+"</p>"
 
 tempSerial = serial.Serial( "/dev/ttyUSB0", baudrate=9600)
 ledSerial = serial.Serial( "/dev/ttyACM0", baudrate=9600)
-#sleep(2)
+#alarmSerial = serial.Serial( "/dev/ttyUSB1", baudrate=9600)
+
 
 #res = device+":"+state
 #ledSerial.write(res)
-
-ledSerial.write(state)
-print "<p> Estat: "+ledSerial.readline()+"</p>"
-
+if (device == "3"):
+	print "<p>DADEVAISISTHREE</p>"
+	m = random.getrandbits(256)
+	m = str(m)
+	hashm = hashlib.sha256(m).hexdigest()
+	#print hashm
+	ledSerial.write(m)
+	hashr = ledSerial.read(32)
+	hashr = hashr.encode('hex')
+	#print hashr
+	if (hashr == hashm):
+		print "<p>OKDAHASH</p>"
+		ledSerial.write(state)
+		state = ledSerial.readline()
+		print "<p>Penispolla</p>"
+	else:
+		print "<p>HAFALLAOELSHASHASHA</p>"
+	print "<p>HASHM: "+hashm+"</p>"
+	print "<p>HASHR: "+hashr+"</p>"
+	#print "<p> Estat: "+ledSerial.readline()+"</p>"
+#elif (device == "5"):
+	#alarmSerial.write(state)
+print "<p>TEVIADARLATEMP</p>"
 tempSerial.write("1") #Ask for the temperature
 tempS = tempSerial.readline()
-print "<p>Temp string: "+tempS+"</p>"
 temp = int(float(tempS)+0.5)
 
 print "<p> Temp: %d</p>"%temp
 
-payload = {"username":"pti", "password":"pti", "deviceID":"2", "newstate":temp}
-r = requests.post("http://fiberparty.upc.es:8080/PTIRestBackend/webresources/devicesstatepi", data = payload)
+#payload = {"username":"pti", "password":"pti", "deviceID":"2", "newstate":temp}
+#r = requests.post("http://fiberparty.upc.es:8080/PTIRestBackend/webresources/devicesstatepi", data = payload)
